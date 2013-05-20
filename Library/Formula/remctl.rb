2353f11affe42a9b1cef581334891057692cf978
Hugh Cole-Baker <sigmaris@gmail.com>
2013-05-20 15:43:48 +0100

    Upgrade remctl to current version (3.4)

--- Library/Formula/remctl.rb
+++ Library/Formula/remctl.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Remctl < Formula
   homepage 'http://www.eyrie.org/~eagle/software/remctl/'
-  url 'http://archives.eyrie.org/software/kerberos/remctl-3.2.tar.gz'
-  sha1 'f49c287c29b6b289995b8907edc6ecba4c298c99'
+  url 'http://archives.eyrie.org/software/kerberos/remctl-3.4.tar.gz'
+  sha1 '0d6757124255e84f5e951986c8a577ef09c6936b'
 
   depends_on 'pcre'
 
